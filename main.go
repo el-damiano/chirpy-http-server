6bf11b3a6a51ed87aedcf0a48cca7caba816ed97
el-damiano <el9damiano@gmail.com>
2025-04-21 15:23:04 +0200

    feat: serve html

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 
 func main() {
 	const port = "8080"
+	const filePath = "."
+
 	serveMux := http.NewServeMux()
+	dir := http.Dir(filePath)
+	serveMux.Handle("/", http.FileServer(dir))
+
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
 	}
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filePath, port)
 	log.Fatal(server.ListenAndServe())
 }
