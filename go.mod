f06f84ea5bc788278d91a00459d593d1a6f63baa
el-damiano <el9damiano@gmail.com>
2025-04-21 14:38:04 +0200

    oh hi!

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/el-damiano/bootdev-http-server
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Handler: serveMux,
+		Addr:    ":" + port,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
