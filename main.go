7a25a4340d01d4fda5144a137738533977e40eea
el-damiano <el9damiano@gmail.com>
2025-04-23 21:07:45 +0200

    feat: add readiness endpoint, change fileserver path

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"net/http"
 )
 
+func ServeReady(writer http.ResponseWriter, request *http.Request) {
+	_ = request
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
 	const filePath = "."
+	dir := http.Dir(filePath)
 
 	serveMux := http.NewServeMux()
-	dir := http.Dir(filePath)
-	serveMux.Handle("/", http.FileServer(dir))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(dir)))
+	serveMux.HandleFunc("/healthz", ServeReady)
 
 	server := &http.Server{
 		Handler: serveMux,
 		Addr:    ":" + port,
 	}
+
 	log.Printf("Serving files from %s on port: %s\n", filePath, port)
 	log.Fatal(server.ListenAndServe())
 }
